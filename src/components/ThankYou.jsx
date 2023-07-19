60c1280a766ae56e083296cbebd2077c4084bde6
LoZo <m.czernik12@gmail.com>
2023-07-19 21:31:28 +0200

    svg change

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
-import ComplitedIcon from "../assets/icon-completed.svg";
+import CompleteIcon from "../assets/icon-complete.svg";
 
 export const ThankYou = ({ backToForm }) => {
   return (
     <div className="thank-you-wrapper">
-      <img src={ComplitedIcon} alt="completed icon" className="comp-img" />
+      <img src={CompleteIcon} alt="completed icon" className="comp-img" />
       <h1 className="comp-title">Thank you!</h1>
       <p className="comp-para">We&apos;ve added your card details</p>
       <button className="back-btn btn" onClick={backToForm}>
